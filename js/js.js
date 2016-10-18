94f0e246574b6cf747c0191a22297f412463ff0c
KCORNN <KCORNN@STANFORD.EDU>
2016-10-18 10:45:34 -0500

    first commit

---
+++ js/js.js
@@ -0,0 +1,24 @@
+//sticky navbar
+$(document).ready(function(){
+    $("#sticker").sticky({topSpacing:0});
+  });
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-default").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-default").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
